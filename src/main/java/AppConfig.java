14a25aa20ebe9a4737f0fb56205b01dddfaa1fc7
mmms2000 <moseskmcmmms@gmail.com>
2025-09-30 11:20:07 +0900

    Cat POJO created

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,3 +1,4 @@
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -11,4 +12,12 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name = "cat")
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public Cat cat() {
+        return new Cat();
+    }
 }
+
+
