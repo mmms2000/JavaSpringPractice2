0e97620deaaca2b47a357b54122570ff9980b730
mmms2000 <moseskmcmmms@gmail.com>
2026-01-12 21:34:51 +0900

    fix annotation from primary to quilifier

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
 
     @Autowired
@@ -18,7 +19,7 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
