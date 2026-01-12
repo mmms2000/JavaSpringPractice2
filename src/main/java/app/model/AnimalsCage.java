ad7d4705955cc12d6ec54c276676a987270dfaa7
mmms2000 <moseskmcmmms@gmail.com>
2026-01-12 14:25:04 +0900

    Add Timer bean and wire it into AnimalsCage

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,6 +1,8 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,11 +11,18 @@ public class AnimalsCage {
     @Autowired
     private Animal animal;
 
+    @Autowired
+    private Timer timer;
+
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
