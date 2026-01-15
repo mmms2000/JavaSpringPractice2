2439f713fc3276b260bf8dace10c2c1a44baa52e
mmms2000 <moseskmcmmms@gmail.com>
2026-01-15 11:24:10 +0900

    test complete

--- src/main/java/feast/models/Goat6.java
+++ src/main/java/feast/models/Goat6.java
@@ -5,11 +5,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Goat6 {
+    private final Horse7 horse7;
+
     @Autowired
-    private Horse7 horse7;
+    public Goat6(Horse7 horse7) {
+        this.horse7 = horse7;
+    }
 
     @Override
     public String toString() {
         return "which is stuffed inside a horse, " + horse7.toString();
     }
+
 }
